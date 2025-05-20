d9ceb463b2585f5411f5eb3f55abcada629a12ea
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-20 15:33:41 -0400

    Time: 28 ms (10.87%) | Memory: 60.2 MB (27.71%) - LeetSync

---
+++ 383-ransom-note/ransom-note.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let magMap = new Map()
+    for(let mag of magazine){
+        magMap.has(mag) ? magMap.set(mag, magMap.get(mag) + 1) : magMap.set(mag, 1)
+    }
+
+    for(let note of ransomNote){
+        if(magMap.has(note) && magMap.get(note) > 0){
+            magMap.set(note, magMap.get(note) + -1)
+
+        }else{
+            return false
+        }
+    }
+
+    console.log("MAGMAP", magMap)
+
+    return true
+    
+};
