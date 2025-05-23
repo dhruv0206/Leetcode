c3bbe494eb29ef9b575f37695def5d1940d03f01
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-23 17:48:47 -0400

    Time: 4 ms (74.26%) | Memory: 58.5 MB (70.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -2,7 +2,6 @@ function isPalindrome(s: string): boolean {
 s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 let i = 0, j = s.length - 1
 while(i<=j){
-    console.log("I J", i, s[i], j, s[j])
     if(s[i] !== s[j]){
         return false
     }
