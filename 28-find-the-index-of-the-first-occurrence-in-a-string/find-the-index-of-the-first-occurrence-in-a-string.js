b26ac062298c560c377515431f272f0ff622013d
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 17:14:44 -0400

    Time: 0 ms (100.00%) | Memory: 53.2 MB (85.76%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let idx = haystack.indexOf(needle)
+    return idx
+    
+};
