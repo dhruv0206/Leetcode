523c3eee085230aa38138fe297d655cc2f73ef15
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 13:58:17 -0400

    Time: 3 ms (54.97%) | Memory: 63 MB (53.94%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let max_area = 0
+    let left = 0, right = height.length - 1
+    while (left < right) {
+        let area = (right - left) * Math.min(height[left], height[right])
+        max_area = Math.max(area, max_area)
+        if (height[left] < height[right]) {
+            left += 1
+        } else {
+            right -= 1
+        }
+    }
+    return max_area
+
+};
