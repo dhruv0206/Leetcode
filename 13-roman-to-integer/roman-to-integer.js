f5efba158002c380f1c975f47a1e88dca1eaf8fb
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 13:05:53 -0400

    Time: 7 ms (31.52%) | Memory: 60.5 MB (79.71%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let sum = 0
+    let i = 0;
+    let n = s.length
+
+    while (i < n) {
+        if (i < n - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            sum += romanMap[s[i + 1]] - romanMap[s[i]]
+            i += 2
+        } else {
+            sum += romanMap[s[i]]
+            i += 1
+        }
+    }
+
+    return sum;
+
+};
