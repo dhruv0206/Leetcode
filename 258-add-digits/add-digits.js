d6ec8148e4282b65c0e1c1e4d2630d8576958578
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 17:02:49 -0400

    Time: 0 ms (100.00%) | Memory: 56.9 MB (67.70%) - LeetSync

--- 258-add-digits/add-digits.js
+++ 258-add-digits/add-digits.js
@@ -2,9 +2,9 @@
  * @param {number} num
  * @return {number}
  */
-var addDigits = function(num) {
+var addDigits = function (num) {
     if (num == 0) {
-            return 0;
-      }
-      return num % 9 == 0 ? 9 : num % 9;
+        return 0;
+    }
+    return num % 9 == 0 ? 9 : num % 9;
 };
