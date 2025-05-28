b614a2a1b29c11e078902da6a8a143d227790297
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 13:25:02 -0400

    Time: 44 ms (51.00%) | Memory: 52.2 MB (99.02%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -28,8 +28,13 @@ var mergeAlternately = function (word1, word2) {
 
     }
 
-// AOTHER APPROACH
-
+    // AOTHER APPROACH
+    // let result = '';
+    //   for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+    //     if (i < word1.length) result += word1[i];
+    //     if (i < word2.length) result += word2[i];
+    //   }
+    //   return result;
 
 
     // console.log("MERGED", merged)
