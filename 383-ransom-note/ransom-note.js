3bb4c639906f32a516c0803c2e5dd1a9d8d4773c
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 11:35:57 -0400

    Time: 26 ms (14.91%) | Memory: 59.5 MB (39.13%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -3,29 +3,29 @@
  * @param {string} magazine
  * @return {boolean}
  */
-// var canConstruct = function(ransomNote, magazine) {
-//     let magMap = new Map()
-//     for(let mag of magazine){
-//         magMap.has(mag) ? magMap.set(mag, magMap.get(mag) + 1) : magMap.set(mag, 1)
-//     }
+var canConstruct = function(ransomNote, magazine) {
+    let magMap = new Map()
+    for(let mag of magazine){
+        magMap.has(mag) ? magMap.set(mag, magMap.get(mag) + 1) : magMap.set(mag, 1)
+    }
 
-//     for(let note of ransomNote){
-//         if(magMap.has(note) && magMap.get(note) > 0){
-//             magMap.set(note, magMap.get(note) - 1)
-//         }else{
-//             return false
-//         }
-//     }
+    for(let note of ransomNote){
+        if(magMap.has(note) && magMap.get(note) > 0){
+            magMap.set(note, magMap.get(note) - 1)
+        }else{
+            return false
+        }
+    }
 
-//     return true
+    return true
     
-// };
-
-var canConstruct = function (ransomNote, magazine) {
-  for (const char of magazine) {
-    ransomNote = ransomNote.replace(char, "");
-  }
-  
-  if (!ransomNote) return true;
-  else return false;
 };
+
+// var canConstruct = function (ransomNote, magazine) {
+//   for (const char of magazine) {
+//     ransomNote = ransomNote.replace(char, "");
+//   }
+  
+//   if (!ransomNote) return true;
+//   else return false;
+// };
