b81776ff79143254288dfa8fb4ed9d56c083355c
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-05-28 16:00:50 -0400

    Time: 1 ms (62.39%) | Memory: 58.5 MB (51.40%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -2,7 +2,13 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getConcatenation = function(nums) {
-    return [...nums, ...nums]
-    
+var getConcatenation = function (nums) {
+    // return [...nums, ...nums]
+    let n = nums.length;
+    let ans = new Array(2 * n);
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+    return ans;
 };
