dd48178eacdcdd8b0789341989089af6d58a5da6
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-01 21:54:38 -0400

    Time: 2 ms (49.58%) | Memory: 53.5 MB (95.46%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -5,26 +5,18 @@
 var calPoints = function(operations) {
     let ans = 0
     let ansStack = []
-    // ansStack.push(operations[0])
-    // const peek = operations[operations.length - 1]
     for(let i = 0; i < operations.length; i++){
-        console.log("OP", operations[i])
         
         if(!isNaN(operations[i])){
-            console.log(operations[i], "IN")
             ansStack.push(parseInt(operations[i]))
             ans+=parseInt(operations[i])
-            console.log("in is nan", ansStack)
         }else if(operations[i] === "+"){
-            console.log("PREVIOUS 1 & 2", ansStack[ansStack.length - 1], ansStack[ansStack.length - 2])
             let previousTwoSums = ansStack[ansStack.length - 1] + ansStack[ansStack.length - 2]
             ans+=previousTwoSums
             ansStack.push(previousTwoSums)
-            console.log("previousTwoSums",previousTwoSums)
         }else if(operations[i] === "C"){
             ans-=ansStack[ansStack.length - 1]
-            ansStack.pop() 
-            console.log("IN C", ansStack)      
+            ansStack.pop()    
 
         }else if(operations[i] === "D"){
             let doubleScore = ansStack[ansStack.length - 1] * 2
@@ -35,8 +27,6 @@ var calPoints = function(operations) {
 
 
     }
-    console.log("ANSWeR", ans)
-    console.log("SSADSASD", ansStack)
     return ans
 };
 
