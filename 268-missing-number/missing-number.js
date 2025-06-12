d90b5f260fc955e11eac7c6caa4803a0f191ca01
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-11 21:08:04 -0400

    Time: 28 ms (14.00%) | Memory: 58.2 MB (20.12%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const numLen = nums.length
+    nums.sort((a,b) => b-a)
+    console.log("NUMS", nums)
+    for(let i = 0; i < numLen; i++){
+        let diff = numLen - nums[i]
+
+        if(diff !== i){
+            return nums[i] + 1
+        }
+    }
+
+    return 0
+    
+};
