b42501887e4c0cb679977aad9d0b3cac51caec4f
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-28 14:06:59 -0400

    Time: 72 ms (7.30%) | Memory: 66.8 MB (95.20%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    // HASH MAP APPROACH
+
+
+    // let numsMap = new Map()
+    // for(let num of nums){
+    //     console.log(numsMap.has(num))
+    //     if (numsMap.has(num)){
+    //         let currCount = numsMap.get(num) + 1
+    //         numsMap.set(num, currCount)
+    //         if(numsMap.get(num) > 1){
+    //             return true
+    //         }
+    //     }else{
+    //         numsMap.set(num, 1)
+    //     }
+
+    // }
+    // return false
+
+    let res = anotherApproach(nums)
+    return res;
+};
+
+function anotherApproach(nums){
+    nums.sort((a, b) => a - b)
+
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] === nums[i-1]){
+            return true
+        }
+    }
+
+    return false
+}
