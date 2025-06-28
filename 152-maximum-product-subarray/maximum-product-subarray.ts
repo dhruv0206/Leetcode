a44a7527ea55533895f3140fa03a3079731be43d
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-28 14:44:51 -0400

    Time: 401 ms (5.21%) | Memory: 58.6 MB (29.28%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.ts
@@ -0,0 +1,17 @@
+function maxProduct(nums: number[]): number {
+    if(nums.length === 1){
+        return nums[0]
+    }
+    let maxProd = 0
+    for (let i = 0; i < nums.length; i++) {
+        let product = 1
+        for (let j = i; j < nums.length; j++) {
+            product *= nums[j]
+            maxProd = Math.max(product, maxProd)
+        }
+    }
+
+    return maxProd
+
+
+};
