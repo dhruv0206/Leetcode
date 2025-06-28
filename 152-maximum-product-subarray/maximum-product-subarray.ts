99d6a46453ff3a83824a66a3ed8a3077ca97775d
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-28 15:53:59 -0400

    Time: 3 ms (62.19%) | Memory: 57 MB (52.98%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.ts
+++ 152-maximum-product-subarray/maximum-product-subarray.ts
@@ -1,19 +1,51 @@
-//  BRUTE FORCE METHOD TO SOLVE
+//  USING PREFIX AND SUFFIX
 
 function maxProduct(nums: number[]): number {
+    let max = 0
+    let prefix = 1
+    let suffix = 1
+    let n = nums.length
+
     if(nums.length === 1){
         return nums[0]
     }
-    let maxProd = -1
-    for (let i = 0; i < nums.length; i++) {
-        let product = 1
-        for (let j = i; j < nums.length; j++) {
-            product *= nums[j]
-            maxProd = Math.max(product, maxProd)
+
+    for(let i = 0 ; i < nums.length; i++){
+        if(prefix == 0){
+            prefix = 1
         }
+        if(suffix == 0){
+            suffix = 1
+        }
+
+        prefix *= nums[i]
+        suffix *= nums[n-i-1]
+
+        max = Math.max(max, Math.max(prefix, suffix))
     }
 
-    return maxProd
-
-
+    return max
 };
+
+
+
+
+//  BRUTE FORCE METHOD TO SOLVE
+
+// function maxProduct(nums: number[]): number {
+//     if(nums.length === 1){
+//         return nums[0]
+//     }
+//     let maxProd = -1
+//     for (let i = 0; i < nums.length; i++) {
+//         let product = 1
+//         for (let j = i; j < nums.length; j++) {
+//             product *= nums[j]
+//             maxProd = Math.max(product, maxProd)
+//         }
+//     }
+
+//     return maxProd
+
+
+// };
