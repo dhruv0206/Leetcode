7e726b3847ca45258ab6a892a880df12b83dcccd
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-28 16:54:14 -0400

    Time: 1 ms (89.19%) | Memory: 63.8 MB (91.46%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -0,0 +1,19 @@
+//kadane's Algo
+
+function maxSubArray(nums: number[]): number {
+    let maxSum = -Infinity
+    let sum = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+
+        if(sum > maxSum){
+            maxSum = sum
+        }
+
+        if(sum < 0){
+            sum = 0
+        }
+    }
+    return maxSum
+};
