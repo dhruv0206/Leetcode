35965cbb26daa2a98fdf40bb8192767b890f3697
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-30 18:10:14 -0400

    Time: 0 ms (100.00%) | Memory: 56 MB (9.66%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,4 +1,22 @@
-function findMin(nums: number[]): number {
+// binary search
 
-    return Math.min(...nums)
+function findMin(nums: number[]): number {
+    let left = 0;
+    let right = nums.length - 1
+
+    while(left < right){
+        const mid = Math.floor((left + right)/2)
+        if(nums[mid] > nums[right]){
+            left = mid + 1
+        }else{
+            right = mid
+        }
+    }
+
+    return nums[left]
 };
+
+
+// function findMin(nums: number[]): number {
+//     return Math.min(...nums)
+// };
