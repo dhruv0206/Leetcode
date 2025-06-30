3f96938ae410ac83702b99112549a6ce48f604ef
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-30 17:52:41 -0400

    Time: 1 ms (99.48%) | Memory: 71.5 MB (50.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -16,3 +16,56 @@ function productExceptSelf(nums: number[]): number[] {
 
     return ans;
 }
+
+
+// ANOTHER BRUTE FORCE APPROACH
+
+/*
+var productExceptSelf = function(nums) {
+    let ans = []
+    let product = 1
+    // for(let i=0; i<nums.length ; i++ ){
+    //     let product = 1
+    //     for(let j = 0; j< nums.length; j++){
+    //         if(i!==j){
+    //             product *= nums[j]
+    //         }    
+    //     }
+    //     ans[i] = product
+    // }
+    let hasZero = false
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i] !== 0){
+            product *= nums[i]
+        }
+        if(nums[i] === 0){
+            if(hasZero){
+                product = 0
+            }
+            hasZero = true
+        }
+    }
+
+    for(let j = 0; j<nums.length; j++){
+        // console.log(product)
+        // console.log(nums[j])
+        if(nums[j] === 0){
+            ans[j] = product
+        } else{
+            if(hasZero){
+                ans[j] = 0
+            }else{
+                ans[j] = product/nums[j]
+
+            }
+
+        }
+        // ans[j] = product/nums[j]
+        // if (product !== 0){
+        // }
+        // console.log("AN", product/nums[j])
+    }
+    return ans
+    
+};
+*/
