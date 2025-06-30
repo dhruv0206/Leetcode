fbe16d2a53d9b8722ad415c17d8971819f111c9b
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-06-30 18:11:03 -0400

    Time: 3 ms (39.26%) | Memory: 64 MB (25.81%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -0,0 +1,15 @@
+function maxArea(height: number[]): number {
+    let max_area = 0
+    let left = 0, right = height.length - 1
+    while (left < right) {
+        let area = (right - left) * Math.min(height[left], height[right])
+        max_area = Math.max(area, max_area)
+        if (height[left] < height[right]) {
+            left += 1
+        } else {
+            right -= 1
+        }
+    }
+    return max_area
+    
+};
