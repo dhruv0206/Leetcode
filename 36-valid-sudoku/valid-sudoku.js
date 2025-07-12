8cedf91edb57ee9cc0d71222bca802dab958dcec
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-12 14:18:07 -0400

    Time: 58 ms (5.01%) | Memory: 64.5 MB (5.41%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -56,6 +56,7 @@ var isValidSudoku = function(board) {
                     return false;
                 }
                 temp.add(currVal);
+                console.log(currVal)
             }
         }
     }
