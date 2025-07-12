ffffd8df98fa5fc6430c3805feabd925cfa66bce
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-12 14:15:11 -0400

    Time: 6 ms (49.71%) | Memory: 57.8 MB (46.57%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -5,6 +5,15 @@
 var isValidSudoku = function(board) {
     let rows = board.length
     let cols = board[0].length
+
+    // If there is n*n matrix
+
+    // let boxSize = Math.sqrt(n);
+    
+    // // Check if n is a perfect square (valid Sudoku dimension)
+    // if (boxSize !== Math.floor(boxSize)) {
+    //     return false; // Invalid Sudoku size
+    // }
     
     // Rows validation
     for(let i = 0; i < board.length; i++){
@@ -52,5 +61,29 @@ var isValidSudoku = function(board) {
             }
         }
     }
+
+    // If there is n*n matrix
+
+    // for(let boxRow = 0; boxRow < boxSize; boxRow++){
+    //     for(let boxCol = 0; boxCol < boxSize; boxCol++){
+    //         let temp = new Set();
+    //         let startRow = boxRow * boxSize;
+    //         let startCol = boxCol * boxSize;
+            
+    //         for(let row = startRow; row < startRow + boxSize; row++){
+    //             for(let col = startCol; col < startCol + boxSize; col++){
+    //                 let currVal = board[row][col];
+    //                 if(currVal !== '.' && temp.has(currVal)){
+    //                     return false;
+    //                 }
+    //                 if(currVal !== '.'){
+    //                     temp.add(currVal);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    
     return true
 };
