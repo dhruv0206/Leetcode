7287f2ec8a84e983814cc85443c1be9ecc1f3523
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-13 15:17:33 -0400

    Time: 813 ms (5.07%) | Memory: 74.7 MB (6.15%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    nums.sort((a, b) => a - b)
+    let z = 0;
+    let i = z + 1;
+    let j = nums.length - 1
+    let resSet = new Set()
+    // if(nums.length === 3){
+    //     if(nums[0] + nums[1] + nums[2] === 0){
+    //         return [nums[0] + nums[1] + nums[2]]
+    //     }
+    // }
+    while (z <= nums.length - 3) {
+        while (i < j) {
+            // console.log("I,j", z, i, j, nums[z], nums[i], nums[j])
+            let sum = nums[z] + nums[i] + nums[j]
+            // console.log("SUM", sum)
+            if (sum === 0) {
+                // console.log("IN RES", z, i, j, nums[z], nums[i], nums[j])
+                // res.every(s => clickedArr.includes(s));
+                // let x = res.some(item => arr2.includes(item))
+                // console.log("XXXX", x)
+                const elementToAddArr = [nums[z], nums[i], nums[j]] 
+                // res = [...res, elementToAddArr]
+                // console.log("res", res)
+                resSet.add(JSON.stringify(elementToAddArr))
+                i++
+                j--
+            }
+            if(sum<0){
+                i++
+            }
+            if(sum>0){
+                j--
+            }
+
+        }
+        z++
+        i = z + 1
+        j = nums.length - 1
+
+    }
+
+    let intArr = Array.from(resSet).map(str => JSON.parse(str));
+
+    // console.log("resSet", intArr)
+    return intArr
+
+};
