de0d15dce92fd5535f1cc332eb202e95e5a01861
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-14 17:54:56 -0400

    Time: 2 ms (74.63%) | Memory: 55.9 MB (49.23%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -2,21 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-     const stack = []; 
-    const hash = { ')': '(', ']': '[', '}': '{' }; 
-    
+var isValid = function (s) {
+    const stack = [];
+    const hash = { ')': '(', ']': '[', '}': '{' };
+
     for (const char of s) {
         if (char in hash) {
             if (stack.length && stack[stack.length - 1] === hash[char]) {
-                stack.pop(); 
+                stack.pop();
             } else {
-                return false; 
+                return false;
             }
         } else {
-            stack.push(char); 
+            stack.push(char);
         }
     }
-    return stack.length === 0; 
-    
+    return stack.length === 0;
+
 };
