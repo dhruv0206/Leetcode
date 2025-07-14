7c9987f414149bbd217d92c1b9d02d8ce3e095ab
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-14 01:48:33 -0400

    Time: 2 ms (83.12%) | Memory: 65.6 MB (15.74%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,21 +2,43 @@
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function (prices) {
+//     let maxp = 0
+//     let minp = Infinity
+
+//     for (let price of prices) {
+//         if (price < minp) {
+//             minp = price
+//         }
+
+//         let profit = price - minp
+//         if (profit > maxp) {
+//             maxp = profit
+//         }
+//     }
+
+//     return maxp
+
+// };
+
+
+// Sliding Window
+
 var maxProfit = function (prices) {
-    let maxp = 0
-    let minp = Infinity
-
-    for (let price of prices) {
-        if (price < minp) {
-            minp = price
+    const n = prices.length;
+    let maxProfit = 0;
+    let i = 0;
+    for (let j = 0; j < n; j++) {
+        if (prices[j] < prices[i]) {
+            i = j;
         }
-
-        let profit = price - minp
-        if (profit > maxp) {
-            maxp = profit
+        else {
+            const diff = prices[j] - prices[i];
+            maxProfit = maxProfit < diff ? diff : maxProfit;
         }
     }
-
-    return maxp
-
+    return maxProfit;
 };
+
+
+
