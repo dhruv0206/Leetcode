d775a4fc636f17c2e4b474858911000ca46d4d73
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-24 10:40:11 -0400

    Time: 148 ms (80.03%) | Memory: 82.5 MB (85.10%) - LeetSync

--- 883-car-fleet/car-fleet.js
+++ 883-car-fleet/car-fleet.js
@@ -12,7 +12,9 @@ var carFleet = function (target, position, speed) {
 
     let stack = [];
     pair.forEach((car, index) => {
-        stack.push((target - car[0]) / car[1]); // formula to calcuate time for each car to reach destination (target - position/speed)
+        const position = car[0]
+        const speed = car[1]
+        stack.push((target - position) / speed); // formula to calcuate time for each car to reach destination (target - position/speed)
         const stackLength = stack.length;
         if (stack.length >= 2 && stack[stackLength - 1] <= stack[stackLength - 2]) {
             stack.pop(); //if cars collide remove the car whose starting position is less. (i.e last pushed car)   
