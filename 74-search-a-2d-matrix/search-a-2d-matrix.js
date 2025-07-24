cee5633fbb060cebc4a2e1b68d65b6515745726a
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-24 18:50:21 -0400

    Time: 0 ms (100.00%) | Memory: 54.1 MB (23.31%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let m = matrix.length // row
-    let n = matrix[0].length // col
+    let rowsLen = matrix.length // row
+    let colsLen = matrix[0].length // col
     let low = 0
-    let high = (m * n) - 1
+    let high = (rowsLen * colsLen) - 1
 
     while(low<= high){
         let mid = Math.floor((low+high)/2)
-        let row = Math.floor(mid/n)
-        let col = Math.floor(mid%n)
+        let row = Math.floor(mid/colsLen)
+        let col = Math.floor(mid%colsLen)
 
         if(target === matrix[row][col]) return true
         else if(target < matrix[row][col]) high = mid - 1
