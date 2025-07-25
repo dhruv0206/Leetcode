57610747501d9f6636cfd8b768c9992ae1bfdb6b
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-25 12:32:26 -0400

    Time: 0 ms (100.00%) | Memory: 53.5 MB (66.70%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -2,6 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
+var findMin = function (nums) {
+    let low = 0, high = nums.length-1, ans = Infinity
+
+    while(low<=high){
+        let mid = Math.floor((low+high)/2)
+
+        if(nums[low] <= nums[mid]){
+            ans = Math.min(ans, nums[low])
+            low = mid + 1
+        }else{
+            ans = Math.min(ans, nums[mid])
+            high = mid - 1
+        }
+    }
+
+    return ans
+
+    
+};
+
+// One solution
+
+/*
 var findMin = function(nums) {
     return Math.min(...nums)
 };
+ */
