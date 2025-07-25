2ce84c9b28dee2a1a74d17eb73b290fc136e5a72
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-25 12:46:32 -0400

    Time: 0 ms (100.00%) | Memory: 54.5 MB (15.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,10 +3,39 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    
-    return nums.indexOf(target)
+var search = function (nums, target) {
+    let low = 0, high = nums.length - 1, ans = Infinity
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+
+        if (nums[mid] === target) return mid;
+
+        // left sorted
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+
+        } else {
+            // right sorted
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+
+    return -1
+
+
+
 };
 
-// one solution
+// one solution easiest
+
+// return nums.indexOf(target)
 
