86c9979996768f4f813c8366f890a4005fd3eb01
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-24 20:33:34 -0400

    Time: 39 ms (6.58%) | Memory: 62.7 MB (5.05%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+    let low = 0, high = Math.max(...piles), ans = Infinity
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        let totalHours = calculateTotalHours(piles, mid)
+        console.log("totalHours",totalHours)
+        if (totalHours <= h){
+             high = mid - 1
+             ans = mid
+        }else {
+            low = mid + 1
+        }
+    }
+
+    return ans;
+
+
+};
+
+const calculateTotalHours = (arr, hourly) => {
+    let totalHours = 0
+    for (let num of arr) {
+        totalHours += Math.ceil(num / hourly)
+    }
+    return totalHours;
+
+}
