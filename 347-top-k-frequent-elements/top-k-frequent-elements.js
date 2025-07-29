9cad78ec81fd017a9b831c7c998464b5011d3b85
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-29 11:57:25 -0400

    Time: 9 ms (80.82%) | Memory: 58.9 MB (69.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -6,12 +6,8 @@
 var topKFrequent = function (nums, k) {
     let countMap = new Map()
 
-    for (let i = 0; i < nums.length; i++) {
-        if (countMap.has(nums[i])) {
-            countMap.set(nums[i], countMap.get(nums[i]) + 1)
-        } else {
-            countMap.set(nums[i], 1)
-        }
+    for (const num of nums) {
+        countMap.set(num, (countMap.get(num) || 0) + 1);
     }
 
     const maxHeap = [...countMap.entries()].sort((a, b) => b[1] - a[1]);
