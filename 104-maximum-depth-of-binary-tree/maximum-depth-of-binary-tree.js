285c30343fa5c142c9df8d706f4899bce9a1b9f7
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-30 11:21:52 -0400

    Time: 0 ms (100.00%) | Memory: 57 MB (73.28%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -11,6 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
+    // DFS
     if(root === null) return 0
 
     let leftDepth = maxDepth(root.left)
@@ -18,3 +19,34 @@ var maxDepth = function(root) {
 
     return 1 + Math.max(leftDepth, rightDepth)
 };
+
+/*
+// BFS
+// Non recursive
+
+if (!root) {
+        return 0;
+    }
+
+    const queue = [];
+    queue.push(root);
+    let depth = 0;
+
+    while (queue.length > 0) {
+        depth++;
+
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+
+    return depth;    
+
+ */
