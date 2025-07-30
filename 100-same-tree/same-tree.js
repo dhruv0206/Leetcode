5d16bc115d3c077b6132a86838fb630a21244ba1
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-30 11:37:26 -0400

    Time: 0 ms (100.00%) | Memory: 54 MB (35.06%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -1,19 +1,24 @@
-var isSameTree = function(p, q) {
-    // Base case: If both trees are empty, they are identical.
-    if (p === null && q === null) {
-        return true;
-    }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    if (p === null && q === null) return true
+    if (p === null || q === null) return false
 
-    // If one of the trees is empty and the other is not, they are not identical.
-    if (p === null || q === null) {
-        return false;
-    }
 
-    // Compare the values of the current nodes.
-    if (p.val !== q.val) {
-        return false;
-    }
+    if (p.val !== q.val) return false
 
-    // Recursively check the left and right subtrees.
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-}
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+
+
+};
