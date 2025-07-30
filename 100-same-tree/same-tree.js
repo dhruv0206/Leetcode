7836d19b91db9aee2bcc8582241335037cd1cac5
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-30 11:37:41 -0400

    Time: 0 ms (100.00%) | Memory: 53.7 MB (60.45%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -12,9 +12,10 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-    if (p === null && q === null) return true
-    if (p === null || q === null) return false
 
+    if (p === null && q === null) return true
+
+    if (p === null || q === null) return false
 
     if (p.val !== q.val) return false
 
