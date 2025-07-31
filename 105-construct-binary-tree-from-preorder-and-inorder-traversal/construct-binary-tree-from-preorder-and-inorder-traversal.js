cbae5f49fc80b0cfae15ba5d8e10c6eeeb07fa68
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-31 12:33:07 -0400

    Time: 74 ms (21.31%) | Memory: 133.3 MB (11.37%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if (!preorder.length) return null;
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
+    let index = inorder.indexOf(rootVal);
+
+    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    return root;
+};
