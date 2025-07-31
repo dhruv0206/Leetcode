2cd539f9911cef87d6473b89828a9d0eda75f7e1
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-31 13:16:50 -0400

    Time: 1 ms (98.27%) | Memory: 65.5 MB (16.76%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
@@ -11,25 +11,17 @@
  * @return {number}
  */
 var maxPathSum = function(root) {
-    let res = -Infinity;
+     let ans = -Infinity;
 
     function dfs(node) {
-        if (!node) {
-            return 0;
-        }
-
-        // Recursively compute the maximum sum of the left and right subtree paths.
-        let leftSum = Math.max(0, dfs(node.left));
-        let rightSum = Math.max(0, dfs(node.right));
-
-        // Update the maximum path sum encountered so far (with split).
-        res = Math.max(res, leftSum + rightSum + node.val);
-
-        // Return the maximum sum of the path (without split).
-        return Math.max(leftSum, rightSum) + node.val;
+        if (!node) return 0;
+        let left = Math.max(dfs(node.left), 0);
+        let right = Math.max(dfs(node.right), 0);
+        ans = Math.max(ans, node.val + left + right);
+        return node.val + Math.max(left, right);
     }
 
     dfs(root);
-    return res;
+    return ans;
     
 };
