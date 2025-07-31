483b5116667beef006eef8af06cd64054675fcf6
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-31 12:34:10 -0400

    Time: 69 ms (28.55%) | Memory: 133.1 MB (13.99%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,12 +12,19 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    if (!preorder.length) return null;
-    let rootVal = preorder[0];
-    let root = new TreeNode(rootVal);
-    let index = inorder.indexOf(rootVal);
+    // if (!preorder.length) return null;
+    // let rootVal = preorder[0];
+    // let root = new TreeNode(rootVal);
+    // let index = inorder.indexOf(rootVal);
 
-    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
-    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
-    return root;
+    // root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    // root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    // return root;
+    if (!preorder.length || !inorder.length) return null;
+  
+  let root = new TreeNode(preorder[0]);
+  let mid = inorder.indexOf(preorder[0]);
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+  return root;
 };
