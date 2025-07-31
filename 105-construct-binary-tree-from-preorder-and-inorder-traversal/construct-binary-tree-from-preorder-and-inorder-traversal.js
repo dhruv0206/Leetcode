5cd43f31a4544db5daa3c447c900a74072425f2a
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-07-31 12:47:47 -0400

    Time: 2 ms (87.71%) | Memory: 58.9 MB (85.30%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -11,20 +11,38 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
-    // if (!preorder.length) return null;
-    // let rootVal = preorder[0];
-    // let root = new TreeNode(rootVal);
-    // let index = inorder.indexOf(rootVal);
+var buildTree = function (preorder, inorder) {
+    let index = 0;
+    const map = new Map();
+    inorder.forEach((val, i) => map.set(val, i));
 
-    // root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
-    // root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
-    // return root;
-    if (!preorder.length || !inorder.length) return null;
-  
-  let root = new TreeNode(preorder[0]);
-  let mid = inorder.indexOf(preorder[0]);
-  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
-  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
-  return root;
+    function helper(start, end) {
+        if (start > end) return null;
+
+        // const rootVal = preorder[index++];
+        const rootVal = preorder[index];
+        index = index + 1;
+        const node = new TreeNode(rootVal);
+        const mid = map.get(rootVal);
+
+        node.left = helper(start, mid - 1);
+        node.right = helper(mid + 1, end);
+        return node;
+    }
+
+    return helper(0, inorder.length - 1);
+
 };
+
+/*
+O(n2)
+
+if (!preorder.length) return null;
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
+    let index = inorder.indexOf(rootVal);
+
+    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    return root;
+ */
