a33649caead093ff56bb3eb253e8bd62f7af3401
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-08-03 14:26:46 -0400

    Time: 101 ms (14.59%) | Memory: 69.4 MB (10.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,44 +2,52 @@
  * @param {character[][]} grid
  * @return {number}
  */
-// var numIslands = function(grid) {
-//     let islands = 0;
-//     const visited = new Set();
-//     const rows = grid.length;
-//     const cols = grid[0].length;
+var numIslands = function(grid) {
+    // BFS
 
-//     const bfs = (r, c) => {
-//         const q = [];
-//         visited.add(`${r},${c}`);
-//         q.push([r, c]);
+    
+    let islands = 0;
+    const visited = new Set();
+    const rows = grid.length;
+    const cols = grid[0].length;
 
-//         while (q.length > 0) {
-//             const [row, col] = q.shift();
-//             const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const bfs = (r, c) => {
+        const q = [];
+        visited.add(`${r},${c}`);
+        q.push([r, c]);
 
-//             for (const [dr, dc] of directions) {
-//                 const nr = row + dr;
-//                 const nc = col + dc;
-//                 if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === "1" && !visited.has(`${nr},${nc}`)) {
-//                     q.push([nr, nc]);
-//                     visited.add(`${nr},${nc}`);
-//                 }
-//             }
-//         }
-//     };
+        while (q.length > 0) {
+            const [row, col] = q.shift();
+            const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-//     for (let r = 0; r < rows; r++) {
-//         for (let c = 0; c < cols; c++) {
-//             if (grid[r][c] === "1" && !visited.has(`${r},${c}`)) {
-//                 islands += 1;
-//                 bfs(r, c);
-//             }
-//         }
-//     }
+            for (const [dr, dc] of directions) {
+                const nr = row + dr;
+                const nc = col + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === "1" && !visited.has(`${nr},${nc}`)) {
+                    q.push([nr, nc]);
+                    visited.add(`${nr},${nc}`);
+                }
+            }
+        }
+    };
 
-//     return islands;    
-// };
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === "1" && !visited.has(`${r},${c}`)) {
+                islands += 1;
+                bfs(r, c);
+            }
+        }
+    }
 
+    return islands;    
+};
+
+
+
+/* 
+
+DFS APproach
 
 const dfs = (grid, r, c) => {
     const rows = grid.length;
@@ -78,3 +86,6 @@ var numIslands = function(grid) {
 
     return islands;
 };
+
+
+*/
