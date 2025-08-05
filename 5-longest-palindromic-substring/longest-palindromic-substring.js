20447f42e3193712638e61c25b0517a0499a4997
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-08-05 11:35:16 -0400

    Time: 18 ms (82.42%) | Memory: 58 MB (41.32%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var longestPalindrome = function (s) {
+    function expandAroundCenter(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+    let longest = "";
+
+    for (let i = 0; i < s.length; i++) {
+        let odd = expandAroundCenter(i, i);
+        let even = expandAroundCenter(i, i + 1);
+
+        if (odd.length > longest.length) {
+            longest = odd;
+        }
+
+        if (even.length > longest.length) {
+            longest = even;
+        }
+    }
+
+    return longest;
+};
+
+
+/*
+claude chat link
+
+https://claude.ai/chat/505d3d0c-1af1-4184-b28a-6b2ee6615d52
+
+
+ */
