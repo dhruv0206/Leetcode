22eacae2fedf1acd0efc2a7819968beeefd84522
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-08-06 12:20:58 -0400

    Time: 3 ms (28.59%) | Memory: 59.2 MB (40.51%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -4,9 +4,6 @@
  */
 var canJump = function(nums) {
     let maxIndex = 0
-    if(nums.length === 1 && nums[0] === 0){
-        return true
-    }
     for(let i = 0; i < nums.length; i++){
         if(i > maxIndex) return false
         maxIndex = Math.max(maxIndex, i + nums[i])
