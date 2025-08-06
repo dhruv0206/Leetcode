58281d04f78f67f6fa455301f316ef665bb5ed65
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-08-06 00:18:18 -0400

    Time: 80 ms (30.38%) | Memory: 64.2 MB (60.76%) - LeetSync

---
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function (nums, k) {
+    const freqMap = new Map();
+    for (const num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    }
+
+    let ans = 0;
+    for (let i = 1; i <= 50; i++) {
+        let sum = 0;
+        for (const num of nums) {
+            if (num === i) sum++;
+            if (num === k) sum--;
+            if (sum < 0) sum = 0;
+            ans = Math.max(ans, sum);
+        }
+    }
+
+    return ans + (freqMap.get(k) || 0);
+};
