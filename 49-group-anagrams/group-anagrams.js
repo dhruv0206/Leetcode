f7ae26cb02f4ff988d4e6d036fd1d5c8b21a7044
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-08-07 18:47:57 -0400

    Time: 24 ms (88.77%) | Memory: 67.8 MB (52.22%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -4,18 +4,33 @@
  */
 var groupAnagrams = function(strs) {
 
-    if(!strs){
-        return [""]
-    }
-    let anagramMap = new Map()
+    // if(!strs){
+    //     return [""]
+    // }
+    // let anagramMap = new Map()
 
-    for(let i = 0; i < strs.length; i++){
-        let sortedKey = strs[i].split('').sort().join('')
-        if(anagramMap.has(sortedKey)){
-            anagramMap.set(sortedKey, [...anagramMap.get(sortedKey), strs[i]])
-        }else{
-            anagramMap.set(sortedKey, [strs[i]])
+    // for(let i = 0; i < strs.length; i++){
+    //     let sortedKey = strs[i].split('').sort().join('')
+    //     if(anagramMap.has(sortedKey)){
+    //         anagramMap.set(sortedKey, [...anagramMap.get(sortedKey), strs[i]])
+    //     }else{
+    //         anagramMap.set(sortedKey, [strs[i]])
+    //     }
+    // }
+    // return [...anagramMap.values()]
+
+    if (!strs || strs.length === 0) return []
+    
+    const anagramMap = new Map()
+    
+    for (const str of strs) {
+        const sortedKey = str.split('').sort().join('') // O(M log M)
+        
+        if (!anagramMap.has(sortedKey)) {
+            anagramMap.set(sortedKey, [])
         }
+        anagramMap.get(sortedKey).push(str)
     }
-    return [...anagramMap.values()]
+    
+    return Array.from(anagramMap.values())
 };
