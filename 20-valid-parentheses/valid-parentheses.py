a7ac2045b2f4761157944b1dc85fab73b9988249
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-09-24 11:30:23 -0400

    Time: 4 ms (10.84%) | Memory: 17.9 MB (34.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -9,7 +9,7 @@ class Solution:
         
         for char in s:
             if char in char_map:
-                if len(stack) > 0 and stack[-1] == char_map.get(char):
+                if stack and stack[-1] == char_map[char]:
                     stack.pop()
                 else:
                     return False
