c773d26d8ce90869c73140d95ee2d1196bda365f
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-10-02 13:48:54 -0400

    Time: 62 ms (72.66%) | Memory: 23.2 MB (21.23%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -4,25 +4,25 @@ import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
-
-        for i in range(len(points)):
-            print(points[i][0], points[i][1])
-            ans = points[i][0]**2 + points[i][1]**2
-            heapq.heappush(heap, (ans, points[i]))
-
-        smallest_tuples = heapq.nsmallest(k, heap)
-        result = [value for key, value in smallest_tuples]
-        return result
-
-
-    
-    
-    # def euclidean_dist(d1, d2):
-    #     origin
-    #     x_sq = (d1[0] - d2[0]) ** 2
-    #     y_sq = (d1[1] - d2[1]) ** 2
         
-    #     ans = math.sqrt(x_s1 + y_s1)
+        for (x, y) in points:
+            dist = -(x*x + y*y)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        
+        return [(x,y) for (dist,x, y) in heap]
 
-    #     return ans
+
+        # heap = []
+
+        # for i in range(len(points)):
+        #     print(points[i][0], points[i][1])
+        #     ans = points[i][0]**2 + points[i][1]**2
+        #     heapq.heappush(heap, (ans, points[i]))
+
+        # smallest_tuples = heapq.nsmallest(k, heap)
+        # result = [value for key, value in smallest_tuples]
+        # return result
         
