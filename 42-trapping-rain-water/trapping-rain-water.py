13c25dfce9160eefbd8e12aea3233baaf60d4893
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-11-05 19:36:21 -0500

    Time: 11 ms (71.49%) | Memory: 19.3 MB (69.26%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,26 +1,42 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        total = 0
-        left = 0
-        right = len(height) - 1
-        lmax = 0
-        rmax = height[right]
+        # total = 0
+        # left = 0
+        # right = len(height) - 1
+        # lmax = 0
+        # rmax = height[right]
 
-        while left < right:
-            if height[left] <= height[right]:
-                if height[left] < lmax:
-                    total += lmax - height[left]
-                else:
-                    lmax = height[left]
-                left +=1
-            else:
-                if height[right] < rmax:
-                    total += rmax - height[right]
-                else:
-                    rmax = height[right]
-                right -=1
+        # while left < right:
+        #     if height[left] <= height[right]:
+        #         if height[left] < lmax:
+        #             total += lmax - height[left]
+        #         else:
+        #             lmax = height[left]
+        #         left +=1
+        #     else:
+        #         if height[right] < rmax:
+        #             total += rmax - height[right]
+        #         else:
+        #             rmax = height[right]
+        #         right -=1
         
-        return total
+        # return total
+        l = 0
+        r = len(height) - 1
+        l_max = 0
+        r_max = 0
+        ans = 0
+
+        while l < r:
+            if height[l] < height[r]:
+                l_max = max(l_max, height[l])
+                ans += l_max - height[l]
+                l += 1
+            else:
+                r_max = max(r_max, height[r])
+                ans += r_max - height[r]
+                r -= 1
+        return ans
 
         
         
