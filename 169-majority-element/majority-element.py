2b598e5346ef7720d444154d0bc843b0baab7e21
dhruv0206 <53010251+dhruv0206@users.noreply.github.com>
2025-11-06 19:32:59 -0500

    Time: 11 ms (29.06%) | Memory: 19.4 MB (19.04%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,18 +1,18 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # m = defaultdict(int)
+        m = defaultdict(int)
         
-        # for num in nums:
-        #     m[num] += 1
+        for num in nums:
+            m[num] += 1
         
-        # n = len(nums) // 2
-        # for key, value in m.items():
-        #     if value > n:
-        #         return key
+        n = len(nums) // 2
+        for key, value in m.items():
+            if value > n:
+                return key
         
-        # return 0
+        return 0
 
-        nums.sort()
-        n = len(nums)
-        return nums[n//2]
+        # nums.sort()
+        # n = len(nums)
+        # return nums[n//2]
